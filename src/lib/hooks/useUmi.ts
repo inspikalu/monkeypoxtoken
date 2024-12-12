e8680407ac786994e1806c86581e629ff6aa9961
donjne <davidjrn247@gmail.com>
2024-12-12 20:57:05 +0100

    added mpl core

--- src/lib/hooks/useUmi.ts
+++ src/lib/hooks/useUmi.ts
@@ -4,7 +4,7 @@ import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
 import { web3JsEddsa } from '@metaplex-foundation/umi-eddsa-web3js';
 import { mplHybrid } from '@metaplex-foundation/mpl-hybrid';
 import { mplToolbox } from '@metaplex-foundation/mpl-toolbox';
-import { mplTokenMetadata } from '@metaplex-foundation/mpl-token-metadata';
+import { mplCore } from '@metaplex-foundation/mpl-core';
 
 export const useUmi = () => {
   return useMemo(() => {
@@ -12,6 +12,6 @@ export const useUmi = () => {
       .use(web3JsEddsa())
       .use(mplHybrid())
       .use(mplToolbox())
-      .use(mplTokenMetadata());
+      .use(mplCore());
   }, []);
 };
